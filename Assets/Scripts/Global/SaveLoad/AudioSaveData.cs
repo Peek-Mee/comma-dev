b8b3888219b0d14e0e8f4dc09f07647df2f0f9b5
HiDE <msyariefhidayatulloh14@gmail.com>
2022-12-15 13:24:45 +0800

    Add SavaLoad module
    - InputSaveData need to be discussed further to match which method to use in setting

---
+++ Assets/Scripts/Global/SaveLoad/AudioSaveData.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using UnityEngine;
+
+namespace Comma.Global.SaveLoad
+{
+    public enum AudioDataType { MASTER, BGM, AMBIANCE, SFX }
+
+    [Serializable]
+    public class AudioSaveData 
+    {
+        [SerializeField] private float _masterVolume;
+        [SerializeField] private float _bgmVolume;
+        [SerializeField] private float _ambianceVolume;
+        [SerializeField] private float _sfxVolume;
+        [SerializeField] private bool _masterMute;
+        [SerializeField] private bool _bgmMute;
+        [SerializeField] private bool _ambianceMute;
+        [SerializeField] private bool _sfxMute;
+
+        public AudioSaveData()
+        {
+            _masterMute = false;
+            _bgmMute = false;
+            _ambianceMute = false;
+            _sfxMute = false;
+
+            _masterVolume = 0f;
+            _bgmVolume = 0f;
+            _ambianceVolume = 0f;
+            _sfxVolume = 0f;
+        }
+        /// <summary>
+        /// Change audio volume setting and set as dirty
+        /// </summary>
+        /// <param name="type">AudioDataType</param>
+        /// <param name="value">float</param>
+        public void ChangeAudioSetting(AudioDataType type, float value)
+        {
+            if (value > 1 || value < 0) return;
+
+            switch(type)
+            {
+                case AudioDataType.MASTER:
+                    _masterVolume = value;
+                    break;
+                case AudioDataType.BGM:
+                    _bgmVolume = value;
+                    break;
+                case AudioDataType.AMBIANCE:
+                    _ambianceVolume = value;
+                    break;
+                case AudioDataType.SFX:
+                    _sfxVolume = value;
+                    break;
+            }
+        }
+        /// <summary>
+        /// Change audio mute setting and set as dirty
+        /// </summary>
+        /// <param name="type">AudioDataType</param>
+        /// <param name="value">bool</param>
+        public void ChangeAudioSetting(AudioDataType type, bool value)
+        {
+            switch (type)
+            {
+                case AudioDataType.MASTER:
+                    _masterMute = value;
+                    break;
+                case AudioDataType.BGM:
+                    _bgmMute = value;
+                    break;
+                case AudioDataType.AMBIANCE:
+                    _ambianceMute = value;
+                    break;
+                case AudioDataType.SFX:
+                    _sfxMute = value;
+                    break;
+            }
+        }
+        /// <summary>
+        /// Get the volume level of a specific audio group type
+        /// </summary>
+        /// <param name="type">AudioDataType</param>
+        /// <returns>float</returns>
+        public float GetVolumeSetting(AudioDataType type)
+        {
+            float temp = type switch
+            {
+                AudioDataType.MASTER => _masterVolume,
+                AudioDataType.BGM => _bgmVolume,
+                AudioDataType.AMBIANCE => _ambianceVolume,
+                AudioDataType.SFX => _sfxVolume,
+                _ => 0f
+            };
+            return temp;
+        }
+        /// <summary>
+        /// Get the mute setting of a specific audio group type
+        /// </summary>
+        /// <param name="type">AudioDataType</param>
+        /// <returns>bool</returns>
+        public bool GetMuteSetting(AudioDataType type)
+        {
+            bool temp = type switch
+            {
+                AudioDataType.MASTER => _masterMute,
+                AudioDataType.BGM => _bgmMute,
+                AudioDataType.AMBIANCE => _ambianceMute,
+                AudioDataType.SFX => _sfxMute,
+                _ => false
+            };
+            return temp;
+        }
+
+    }
+}
---
+++ Assets/Scripts/Global/SaveLoad/VideoSaveData.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+
+namespace Comma.Global.SaveLoad
+{
+    public enum VideoResolutionType {P480, P720, P1080, P1920, P2080}
+    [Serializable]
+    public class VideoSaveData
+    {
+        [SerializeField] private Resolution _displayResolution;
+        [SerializeField] private bool _fullScreen;
+
+        public VideoSaveData()
+        {
+            
+            Display mainDisplay = Display.main;
+            _displayResolution = new()
+            {
+                height = mainDisplay.systemHeight,
+                width = mainDisplay.systemWidth,
+                refreshRate = 60
+            };
+        }
+
+        /// <summary>
+        /// Get saved display resolution
+        /// </summary>
+        /// <returns>Resolution</returns>
+        public Resolution GetDisplayResolution() { return _displayResolution; }
+        /// <summary>
+        /// Save a new display resolution and set data dirty
+        /// </summary>
+        /// <param name="value">Resolution</param>
+        public void SetDisplayResolution(Resolution displayResolution) { _displayResolution = displayResolution; }
+        /// <summary>
+        /// Is game in full screen?
+        /// </summary>
+        /// <returns>bool</returns>
+        public bool IsFullScreen() { return _fullScreen; }
+        /// <summary>
+        /// Set new full screen preference
+        /// </summary>
+        /// <param name="value">bool</param>
+        public void SetFullScreen(bool isFullScreen) { _fullScreen = isFullScreen; }
+    }
+}
