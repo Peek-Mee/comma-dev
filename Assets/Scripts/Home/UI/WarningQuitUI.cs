d01219e73f25b4ccbda72b024112af75a84bf6e0
diotsani <dio.tsani123@gmail.com>
2022-12-15 19:50:46 +0700

    add home ui

---
+++ Assets/Scripts/Home/UI/WarningQuitUI.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Comma.Home.UI
+{
+    public class WarningQuitUI : MonoBehaviour
+    {
+        [Header("Warning Quit Buttons")]
+        [SerializeField] private Button _yesButton;
+        [SerializeField] private Button _noButton;
+        
+        private void OnEnable()
+        {
+            RemoveAllButtonListeners();
+            _yesButton.onClick.AddListener(OnYesButton);
+            _noButton.onClick.AddListener(OnNoButton);
+        }
+
+        private void OnDisable()
+        {
+            RemoveAllButtonListeners();
+        }
+        private void RemoveAllButtonListeners()
+        {
+            _yesButton.onClick.RemoveAllListeners();
+            _noButton.onClick.RemoveAllListeners();
+        }
+        private void OnYesButton()
+        {
+            Application.Quit();
+        }
+        private void OnNoButton()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
