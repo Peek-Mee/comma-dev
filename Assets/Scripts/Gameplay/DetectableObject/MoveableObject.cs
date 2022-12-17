0ca94400d002c1d928f1bbc14bd5027cda32e90b
HiDE <msyariefhidayatulloh14@gmail.com>
2022-12-17 19:51:55 +0800

    Add Portal and Orb
    Moveable object still a placeholder for the next sprint

---
+++ Assets/Scripts/Gameplay/DetectableObject/MoveableObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Comma.Gameplay.DetectableObject
+{
+    public class MoveableObject : MonoBehaviour, IDetectable
+    {
+        [SerializeField] private string _objectId;
+
+        public string GetObjectId()
+        {
+            return _objectId;
+        }
+
+        public Vector3 GetPosition()
+        {
+            return transform.position;
+        }
+
+        public void Interact()
+        {
+            return;
+        }
+    }
+}
