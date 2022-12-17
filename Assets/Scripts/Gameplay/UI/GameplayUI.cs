0abc2ea594368fbba7c7f029c86609f0c8edbf76
diotsani <dio.tsani123@gmail.com>
2022-12-17 21:08:20 +0700

    add UI gameplay

---
+++ Assets/Scripts/Gameplay/UI/GameplayUI.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace Comma.Gameplay.UI
+{
+    public class GameplayUI : MonoBehaviour
+    {
+        [SerializeField] private GameObject _pausePanel;
+
+        private void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.Escape))
+            {
+                _pausePanel.SetActive(!_pausePanel.activeSelf);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Gameplay/UI/WarningMenuUI.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Comma.Gameplay.UI
+{
+    public class WarningMenuUI : MonoBehaviour
+    {
+        [Header("Warning Menu Buttons")]
+        [SerializeField] private Button _yesButton;
+        [SerializeField] private Button _noButton;
+        
+        [Header("Scene Management")]
+        [SerializeField] private string _gameplaySceneName = "Gameplay";
+        
+        private void OnEnable()
+        {
+            RemoveAllButtonListeners();
+            _yesButton.onClick.AddListener(OnYesButton);
+            _noButton.onClick.AddListener(OnNoButton);
+        }
+
+        private void OnDisable()
+        {
+            RemoveAllButtonListeners();
+        }
+        private void RemoveAllButtonListeners()
+        {
+            _yesButton.onClick.RemoveAllListeners();
+            _noButton.onClick.RemoveAllListeners();
+        }
+        private void OnYesButton()
+        {
+            SceneManager.LoadScene(_gameplaySceneName);
+        }
+        private void OnNoButton()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
