6b7f0ab6807cea2595897297824016efa6788ada
diotsani <dio.tsani123@gmail.com>
2022-12-21 00:03:08 +0700

    add confirm setting ui

---
+++ Assets/Scripts/Gameplay/UI/ConfirmSettingUI.cs
@@ -0,0 +1,41 @@
+﻿using Comma.Global.SaveLoad;
+using Comma.Global.Settings;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Gameplay.UI
+{
+    public class ConfirmSettingUI : MonoBehaviour
+    {
+        [Header("Confirm Setting Buttons")]
+        [SerializeField] private Button _yesButton;
+        [SerializeField] private Button _noButton;
+        
+        private void OnEnable()
+        {
+            RemoveAllButtonListeners();
+            _yesButton.onClick.AddListener(OnYesButton);
+            _noButton.onClick.AddListener(OnNoButton);
+        }
+
+        private void OnDisable()
+        {
+            RemoveAllButtonListeners();
+        }
+        private void RemoveAllButtonListeners()
+        {
+            _yesButton.onClick.RemoveAllListeners();
+            _noButton.onClick.RemoveAllListeners();
+        }
+        private void OnYesButton()
+        {
+            SaveSystem.SaveDataToDisk();
+            gameObject.SetActive(false);
+        }
+        private void OnNoButton()
+        {
+            VideoSetting.VideoSettingInstance.CancelVideoSetting();
+            gameObject.SetActive(false);
+        }
+    }
+}
--- Assets/Scripts/Gameplay/UI/OptionsUI.cs
+++ Assets/Scripts/Gameplay/UI/OptionsUI.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Comma.Global.SaveLoad;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,26 +7,52 @@ namespace Comma.Gameplay.UI
 {
     public class OptionsUI : MonoBehaviour
     {
+        [SerializeField] private GameObject _confirmSettingPopUp;
+        
         [Header("Options Button")]
         [SerializeField] private Button _backButton;
-
+        
         private void OnEnable()
         {
             RemoveAllButtonListeners();
             _backButton.onClick.AddListener(OnBackButton);
         }
-
         private void OnDisable()
         {
             RemoveAllButtonListeners();
         }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if(gameObject.activeSelf)
+                    CheckConfirmSetting();
+            }
+        }
+
         private void RemoveAllButtonListeners()
         {
             _backButton.onClick.RemoveAllListeners();
         }
         private void OnBackButton()
         {
-            gameObject.SetActive(false);
+            CheckConfirmSetting();
+        }
+        private void CheckConfirmSetting()
+        {
+            var isConfirm = SaveSystem.SaveDataToDisk();
+            Debug.Log(isConfirm);
+            if (isConfirm)
+            {
+                _confirmSettingPopUp.SetActive(true);
+                gameObject.SetActive(false);
+            }
+            else
+            {
+                _confirmSettingPopUp.SetActive(false);
+                gameObject.SetActive(false);
+            }
         }
     }
 }
