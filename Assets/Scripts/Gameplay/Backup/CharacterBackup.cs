c98dd91cd716b5d8b1ed5e7d7cb8360ddf7debf3
diotsani <dio.tsani123@gmail.com>
2022-12-24 20:29:20 +0700

    update

--- Assets/Scripts/Gameplay/Backup/CharacterBackup.cs
+++ Assets/Scripts/Gameplay/Backup/CharacterBackup.cs
@@ -6,10 +6,13 @@ namespace Comma.Gameplay.Backup
     public class CharacterBackup : MonoBehaviour
     {
         private Rigidbody2D _rigidbody2D;
-        
+
         [Header("Movement")]
+        [SerializeField] private Transform _topdownCheck;
+        [SerializeField] private float _topdownDistance;
         [SerializeField] private float _speed;
         private float horizontalInput;
+        private float verticalInput;
         private bool _isFacingRight = true;
         
         [Header("Jump")]
@@ -25,6 +28,8 @@ namespace Comma.Gameplay.Backup
         private void Update()
         {
             horizontalInput = Input.GetAxis("Horizontal");
+            verticalInput = Input.GetAxis("Vertical");
+            
             OnJump();
             if(_isFacingRight && horizontalInput < 0)
             {
@@ -39,13 +44,18 @@ namespace Comma.Gameplay.Backup
         private void FixedUpdate()
         {
             OnMove();
+            OnMoveTopDown();
         }
 
         private void OnMove()
         {
             _rigidbody2D.velocity = new Vector2(horizontalInput * _speed, _rigidbody2D.velocity.y);
         }
-
+        private void OnMoveTopDown()
+        {
+            if(IsTopDown()) 
+                _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, verticalInput * _speed);
+        }
         private void OnJump()
         {
             if(Input.GetKeyDown(KeyCode.Space) && IsGrounded())
@@ -64,6 +74,12 @@ namespace Comma.Gameplay.Backup
         private bool IsGrounded()
         {
             return Physics2D.Raycast(_groundCheck.position, Vector2.down, _groundDistance, LayerMask.GetMask("Ground"));
+        }
+        
+        private bool IsTopDown()
+        {
+            
+            return Physics2D.Raycast(_topdownCheck.position, Vector2.zero, _topdownDistance, LayerMask.GetMask("Ground"));
         }
     }
 }
