a13fd9b124e6c43335053a0d78e1d78b0060ed6f
HiDE <msyariefhidayatulloh14@gmail.com>
2022-12-29 08:31:30 +0800

    Add initial ability to swap between layers

--- Assets/Scripts/Gameplay/Player/PlayerMovement.cs
+++ Assets/Scripts/Gameplay/Player/PlayerMovement.cs
@@ -12,9 +12,32 @@ namespace Comma.Gameplay.Player
         Jump,
         Fall,
     }
+
+    [RequireComponent(typeof(Rigidbody2D), typeof(SpriteRenderer))]
     public class PlayerMovement : MonoBehaviour
     {
+        [Header("Collider Checks")]
+        [SerializeField]
+        [Range(0.2f, 0.35f)]
+        private float _checkRadius = .25f;
+        [SerializeField]
+        private Transform _ground;
+        [SerializeField]
+        private Transform _ceil;
+        [SerializeField]
+        private Transform _rightHand;
+        [SerializeField]
+        private Transform _leftHand;
+        [SerializeField]
+        private LayerMask[] _groundLayers;
+        private int _currentLayer = 0;
+
+        // Private
         private Rigidbody2D _rigidbody2D;
+        private SpriteRenderer _playerSprite;
+        private bool _isInCutScene = false;
+        private bool _isFlipProhibited = false;
+        // #######
 
         [Header("Movement")]
         [SerializeField] private float _normalSpeed;
@@ -27,8 +50,8 @@ namespace Comma.Gameplay.Player
         private bool _isFacingRight = true;
         
         [Header("Jump")]
-        [SerializeField] private Transform _groundCheck;
-        [SerializeField] private float _groundDistance;
+        //[SerializeField] private Transform _groundCheck;
+        //[SerializeField] private float _groundDistance;
         [SerializeField] private float _jumpForce;
         [SerializeField] private float _fallMultiplier;
         
@@ -48,10 +71,17 @@ namespace Comma.Gameplay.Player
         {
             _rigidbody2D = GetComponent<Rigidbody2D>();
             _animator = GetComponent<Animator>();
+            _playerSprite = GetComponent<SpriteRenderer>();
         }
 
         private void Update()
         {
+            //print($"Layer 1: {_groundLayers[0].value}");
+            //print($"Layer 2: {_groundLayers[1].value}");
+            if (!Physics2D.GetIgnoreLayerCollision(3, 7))
+            {
+                Physics2D.IgnoreLayerCollision(3, 7, true);
+            }
             horizontalInput = Input.GetAxis("Horizontal");
             verticalInput = Input.GetAxis("Vertical");
             _isWalking = Mathf.Abs(horizontalInput) > 0.1f;
@@ -86,6 +116,11 @@ namespace Comma.Gameplay.Player
                 if (_rigidbody2D.velocity.y > 0)
                 {
                     _playerState = PlayerState.Jump;
+                    print("Hello");
+                    if (EligibleToSwapLayer())
+                    {
+                        SwapLayer();
+                    }
                 }
                 else
                 {
@@ -156,21 +191,63 @@ namespace Comma.Gameplay.Player
 
         private void OnFlip()
         {
-            if(_isFacingRight && horizontalInput < 0)
-            {
-                _isFacingRight = !_isFacingRight;
-                Vector3 scale = transform.localScale;
-                scale.x *= -1;
-                transform.localScale = scale;
-            }
-            else if(!_isFacingRight && horizontalInput > 0)
-            {
-                _isFacingRight = !_isFacingRight;
-                Vector3 scale = transform.localScale;
-                scale.x *= -1;
-                transform.localScale = scale;
-            }
+            //if(_isFacingRight && horizontalInput < 0)
+            //{
+            //    _isFacingRight = !_isFacingRight;
+            //    Vector3 scale = transform.localScale;
+            //    scale.x *= -1;
+            //    transform.localScale = scale;
+            //}
+            //else if(!_isFacingRight && horizontalInput > 0)
+            //{
+            //    _isFacingRight = !_isFacingRight;
+            //    Vector3 scale = transform.localScale;
+            //    scale.x *= -1;
+            //    transform.localScale = scale;
+            //}
+            if (_isFlipProhibited) return;
+            _playerSprite.flipX = !_isFacingRight;
         }
+        #region SwapLayerMask
+        private bool EligibleToSwapLayer()
+        {
+            int layerToCheck;
+            _ = _currentLayer == 0 ? layerToCheck = 1 :
+                layerToCheck = 0;
+            Physics2D.IgnoreLayerCollision(3, 7, false);
+            RaycastHit2D hit = Physics2D.Raycast(_ground.position,
+                Vector2.down, _checkRadius, _groundLayers[layerToCheck]);
+            print(!hit.collider);
+            if (hit.collider == null) return true;
+            else return false;
+        }
+
+        private void SwapLayer()
+        {
+            _ = _currentLayer == 0 ? _currentLayer = 1 :
+                _currentLayer = 0;
+
+            gameObject.layer = BitToLayer(_groundLayers[_currentLayer]);
+        }
+
+        private void OnLayerEdge()
+        {
+            SwapLayer();
+        }
+
+        private int BitToLayer(int bitmask)
+        {
+            int res = bitmask > 0 ? 0 : 31;
+            while(bitmask > 1)
+            {
+                bitmask >>= 1;
+                res++;
+            }
+            return res;
+        }
+        #endregion
+
+        #region Animation
         private void IdleAnimation(bool isIdle)
         {
             _animator.SetBool(On_Idle, isIdle);
@@ -188,9 +265,11 @@ namespace Comma.Gameplay.Player
         {
             _animator.SetBool(On_Fall, isFalling);
         }
+        #endregion
+
         private bool IsGrounded()
         {
-            return Physics2D.Raycast(_groundCheck.position, Vector2.down, _groundDistance, LayerMask.GetMask("Ground"));
+            return Physics2D.Raycast(_ground.position, Vector2.down, _checkRadius, _groundLayers[_currentLayer]);
         }
         
     }
