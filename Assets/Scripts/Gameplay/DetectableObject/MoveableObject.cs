b4c586da9dd72404227539e26f01c3d3a2d926d6
diotsani <dio.tsani123@gmail.com>
2023-01-11 23:02:48 +0700

    trigger using raycast

--- Assets/Scripts/Gameplay/DetectableObject/MoveableObject.cs
+++ Assets/Scripts/Gameplay/DetectableObject/MoveableObject.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
+using Comma.Global.PubSub;
 using UnityEngine;
 
 namespace Comma.Gameplay.DetectableObject
@@ -6,7 +8,7 @@ namespace Comma.Gameplay.DetectableObject
     public class MoveableObject : MonoBehaviour, IDetectable
     {
         [SerializeField] private string _objectId;
-
+        private bool _isInteracted;
         public string GetObjectId()
         {
             return _objectId;
@@ -19,7 +21,38 @@ namespace Comma.Gameplay.DetectableObject
 
         public void Interact()
         {
+            _isInteracted = true;
             return;
+        }
+
+        public void UnInteract()
+        {
+            _isInteracted = false;
+        }
+        private float _horizontalUserInput;
+        private Rigidbody2D _rigidbody2D;
+        [SerializeField] private float _speed;
+        private void Start()
+        {
+            _rigidbody2D = GetComponent<Rigidbody2D>();
+            EventConnector.Subscribe("OnPlayerMove", new(OnMoveInput));
+        }
+
+        private void FixedUpdate()
+        {
+            if (_isInteracted)
+            {
+                var vel = _rigidbody2D.velocity;
+                vel.x = _horizontalUserInput * _speed;
+                _rigidbody2D.velocity = vel;
+            }
+        }
+
+        private void OnMoveInput(object message)
+        {
+            OnPlayerMove msg = (OnPlayerMove)message;
+            print(msg.Direction);
+            _horizontalUserInput = msg.Direction.x;
         }
     }
 }
--- Assets/Scripts/Gameplay/Player/MoveableDetection.cs
+++ Assets/Scripts/Gameplay/Player/MoveableDetection.cs
@@ -1,12 +1,67 @@
-﻿using Comma.Gameplay.DetectableObject;
+﻿using System;
+using Comma.Gameplay.DetectableObject;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Comma.Gameplay.Player
 {
     [RequireComponent(typeof(Collider2D))]
     public class MoveableDetection : MonoBehaviour
     {
-        private bool isHoldingObject = false;
+        private bool isHoldingObject = false; 
+        [SerializeField] private LayerMask _layerMask; 
+        [SerializeField] private float _distance,_radius;
+
+        private void Update()
+        {
+            OnHitMoveable();
+        }
+
+        private void OnHitMoveable()
+        {
+            var rightDirection = new Vector2(transform.position.x + _distance, transform.position.y);
+            var leftDirection = new Vector2(transform.position.x - _distance, transform.position.y);
+            RaycastHit2D rightHit = Physics2D.CircleCast(rightDirection, _radius, rightDirection,0,_layerMask);
+            RaycastHit2D leftHit = Physics2D.CircleCast(leftDirection, _radius, leftDirection,0,_layerMask);
+            
+            if(rightHit.collider != null && rightHit.collider.CompareTag("Moveable"))
+            {
+                GetInput(rightHit.collider);
+                Debug.Log("Right hand detection Moveable "+rightHit.collider.gameObject.name);
+            }
+            else if(leftHit.collider != null && leftHit.collider.CompareTag("Moveable"))
+            {
+                GetInput(leftHit.collider);
+                Debug.Log("Left hand detection Moveable "+leftHit.collider.gameObject.name);
+            }
+        }
+        private void GetInput(Collider2D col)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                if (isHoldingObject)
+                {
+                    isHoldingObject = false;
+                    var moveable = col.GetComponent<MoveableObject>();
+                    moveable.UnInteract();
+                }
+                else
+                {
+                    isHoldingObject = true;
+                    IDetectable detectable = col.gameObject.GetComponent<IDetectable>();
+                    detectable?.Interact();
+                }
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            var rightDirection = new Vector2(transform.position.x + _distance, transform.position.y);
+            var leftDirection = new Vector2(transform.position.x - _distance, transform.position.y);
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(rightDirection, _radius);
+            Gizmos.DrawWireSphere(leftDirection,_radius);
+        }
 
         private void OnTriggerEnter2D(Collider2D collision)
         {
@@ -23,7 +78,7 @@ namespace Comma.Gameplay.Player
                         IDetectable coll = collision.gameObject.GetComponent<IDetectable>();
                         coll?.Interact();
                     }
-
+            
                 }
             }
         }
