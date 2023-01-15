a3e0c5d65bd5d992d76308158e38967a533e73b9
HiDE <msyariefhidayatulloh14@gmail.com>
2023-01-15 18:05:09 +0700

    Still Not finish yet, but now the zoom is correct

--- Assets/Scripts/Gameplay/Environment/CameraChangeTrigger.cs
+++ Assets/Scripts/Gameplay/Environment/CameraChangeTrigger.cs
@@ -8,14 +8,22 @@ namespace Comma.Gameplay.Environment
     public struct CameraChangeData
     {
         [SerializeField] private float _scaleFactor;
-        [SerializeField] private float _transitionTime;
+        [SerializeField] private float _zoomSpeed;
         [SerializeField] private Vector2 _cameraOffset;
         [SerializeField] private bool _isNull;
 
         public float ScaleFactor => _scaleFactor;
-        public float TransitionTime => _transitionTime;
+        public float ZoomSpeed => _zoomSpeed;
         public Vector2 Offset => _cameraOffset;
         public bool IsNull => _isNull;
+
+        public CameraChangeData(float factor, float speed, Vector2 offset, bool isNull)
+        {
+            _scaleFactor = factor;
+            _zoomSpeed = speed;
+            _cameraOffset = offset;
+            _isNull = isNull;
+        }
     }
 
     [RequireComponent(typeof(Collider2D))]
@@ -23,16 +31,24 @@ namespace Comma.Gameplay.Environment
     {
         [SerializeField] private CameraChangeData _onEnter;
         [SerializeField] private CameraChangeData _onExit;
+        [SerializeField] private bool _fromLeft;
         private void OnTriggerEnter2D(Collider2D collision)
         {
             if (!collision.CompareTag("Player")) return;
-            OnTriggerCamera(_onEnter);
+            Vector2 normal = collision.transform.position - transform.position;
+            bool isFromLeft = normal.x > 0 ? true : false;
+            float directedScale = _fromLeft == isFromLeft ? _onEnter.ScaleFactor : _onEnter.ScaleFactor * -1;
+
+
+            CameraChangeData newData = new(
+                directedScale, _onEnter.ZoomSpeed, _onEnter.Offset, false);
+            OnTriggerCamera(newData);
         }
-        private void OnTriggerExit2D(Collider2D collision)
-        {
-            if (!collision.CompareTag("Player")) return;
-            OnTriggerCamera(_onExit);
-        }
+        //private void OnTriggerExit2D(Collider2D collision)
+        //{
+        //    if (!collision.CompareTag("Player")) return;
+        //    OnTriggerCamera(_onExit);
+        //}
 
         private void OnTriggerCamera(CameraChangeData data)
         {
--- Assets/Scripts/Gameplay/Environment/CineCameraController.cs
+++ Assets/Scripts/Gameplay/Environment/CineCameraController.cs
@@ -1,4 +1,5 @@
 ﻿using Cinemachine;
+using Comma.Global.PubSub;
 using Comma.Utility.Collections;
 using System;
 using UnityEngine;
@@ -15,19 +16,60 @@ namespace Comma.Gameplay.Environment
         private int _scaleDirection;
 
         // transition
-        private float _isTransition;
-        private float _currentScale;
+        private bool _isTransition;
+        private float _currentSize;
+        private float _targetSize;
+        private float _zoomSpeed;
 
 
         private void Awake()
         {
             _camera= GetComponent<CinemachineVirtualCamera>();
             _defaultScale = _camera.m_Lens.OrthographicSize;
-            _currentScale = _defaultScale;
+            _currentSize = _defaultScale;
             _offset = _camera.GetCinemachineComponent<CinemachineFramingTransposer>().m_TrackedObjectOffset;
         }
+        private void Start()
+        {
+            EventConnector.Subscribe("OnPlayerMove", new(OnMoveInput));
+            EventConnector.Subscribe("OnCameraChangeTrigger", new(OnCameraChange));
+        }
+        #region PubSub
+        private void OnMoveInput(object msg)
+        {
+            OnPlayerMove message = (OnPlayerMove)msg;
+            _playerDirection = (int)message.Direction.x;
+        }
+        private void OnCameraChange(object msg)
+        {
+            OnCameraChangeTrigger message = (OnCameraChangeTrigger)msg;
+            CameraChangeData data = message.Data;
+            if (data.IsNull) return;
+
+            _targetSize = data.ScaleFactor * _defaultScale;
+            _scaleDirection = _targetSize > 0 ? 1 : -1; 
+            _zoomSpeed= data.ZoomSpeed;
+            _isTransition= true;
+        }
+
+        #endregion
 
         #region Transition Handler
+        private void Update()
+        {
+            if (!_isTransition) return;
+            if (_playerDirection == 0) return;
+            if (Mathf.Abs(_targetSize - _currentSize) < .1f)
+            {
+                _isTransition = false;
+                return;
+            }
+            _camera.m_Lens.OrthographicSize +=  _scaleDirection * _playerDirection * _zoomSpeed * Time.deltaTime;
+            _currentSize = _camera.m_Lens.OrthographicSize;
+
+            //_camera.m_Lens.OrthographicSize = Mathf.Lerp(_camera.m_Lens.OrthographicSize,
+            //    _targetSize, _zoomSpeed);
+        }
         private void HandleTransitionCamera(float newSize)
         {
             _camera.m_Lens.OrthographicSize = newSize;
@@ -38,7 +80,9 @@ namespace Comma.Gameplay.Environment
         {
             string returner = "\n<b>CineCamera Controller</b>\n";
             returner += $"Transition: <i>{_isTransition}</i>\n";
-            returner += $"Scale: <i>{_currentScale / _defaultScale}</i>\n";
+            returner += $"Current: <i>{_currentSize}</i>\n";
+            returner += $"Speed: <i>{_zoomSpeed}</i>\n";
+            returner += $"Target: <i>{_targetSize}</i>\n";
 
             return returner;
         }
