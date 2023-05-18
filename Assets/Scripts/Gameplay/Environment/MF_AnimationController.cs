5eddf11651065286a7c62dc9365971da116f61f7
Muhammad Dio Tsani Iba <dio.tsani123@gmail.com>
2023-05-18 23:08:21 +0700

    update auto jump & animation
    - sulit mengatur value

--- Assets/Scripts/Gameplay/Environment/MF_AnimationController.cs
+++ Assets/Scripts/Gameplay/Environment/MF_AnimationController.cs
@@ -9,7 +9,6 @@ public enum MF_AnimationState
     Walk,
     Run,
     Jump,
-    Land,
 }
 public class MF_AnimationController : MonoBehaviour
 {
@@ -20,12 +19,16 @@ public class MF_AnimationController : MonoBehaviour
     [Range(0, 1)] [SerializeField] private float grounded;
     [Range(0, 2)] [SerializeField] private float movement;
     [Range(0, 2)] [SerializeField] private float xMove;
+    [SerializeField] private float yMove;
     [SerializeField] private float maxStartRun;
+    private bool isLanding;
 
     [Header("Animation Name Parameter")]
     private const string groundedName = "Grounded";
     private const string movementName = "Movement";
     private const string xMoveName = "XMove";
+    private const string yMoveName = "YMove";
+    private const string isLandingName = "IsLanding";
 
     private void Update()
     {
@@ -33,6 +36,8 @@ public class MF_AnimationController : MonoBehaviour
         animator.SetFloat(groundedName, grounded);
         animator.SetFloat(movementName, movement);
         animator.SetFloat(xMoveName, xMove);
+        animator.SetFloat(yMoveName, yMove);
+        animator.SetBool(isLandingName, isLanding);
     }
 
     private void UpdateAnimationState()
@@ -48,28 +53,39 @@ public class MF_AnimationController : MonoBehaviour
 
     private void IdleState()
     {
+        grounded = 0;
         movement = 0;
         xMove = 0;
+        yMove = 0;
     }
     private void WalkState()
     {
+        grounded = 0;
         movement = 1;
+        yMove = 0;
         if(xMove < 1) xMove += Time.deltaTime * 1;
     }
     private void RunState()
     {
+        grounded = 0;
         movement = 2;
+        yMove = 0;
         if(xMove <maxStartRun) xMove += Time.deltaTime * 1;
         else xMove = 2;
     }
 
     private void JumpState()
     {
+        grounded = 1;
         JumpAround();
+        
     }
     
     [Header("Jump Parameter")]
     [SerializeField] private float jumpForce;
+    [SerializeField] private bool isAiring = false;
+    [SerializeField] private float waitJump;
+    [SerializeField] private float waitLand;
     [SerializeField] bool groundedCheck;
     [SerializeField] float groundDistance; 
     [SerializeField] private LayerMask groundMask;
@@ -78,7 +94,42 @@ public class MF_AnimationController : MonoBehaviour
     private void JumpAround()
     {
         groundedCheck = Physics2D.Raycast(transform.position, Vector2.down, groundDistance, groundMask);
-        if (!groundedCheck) return;
-        rb.velocity = Vector2.up * jumpForce;
+        // float min = 0;
+        // float max = 1.52f;
+        // float duration = 1f;
+        // yMove = Mathf.Lerp(min, max, Mathf.PingPong(Time.time / duration, 1));
+        if (groundedCheck)
+        {
+            if (isAiring)
+            {
+                yMove = 0.01f;
+                isLanding = true;
+                StartCoroutine(WaitLanding());
+            }
+            else
+            { 
+                yMove += Time.deltaTime * 1; 
+                StartCoroutine(WaitJump());
+            }
+        }
+        else
+        {
+            
+        }
+
+        IEnumerator WaitLanding()
+        {
+            yield return new WaitForSeconds(waitLand);
+            isLanding = false;
+            isAiring = false;
+            
+        }
+        IEnumerator WaitJump()
+        {
+            yield return new WaitForSeconds(waitJump);
+            rb.velocity = Vector2.up * jumpForce;
+            yield return new WaitForSeconds(0.2f);
+            isAiring = true;
+        }
     }
 }
